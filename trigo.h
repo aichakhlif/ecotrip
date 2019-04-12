8e22eecea7aefaa92c0453ab9c69e89206c966b8
hamdyhass <47847632+hamdyhass@users.noreply.github.com>
2019-04-12 11:32:33 +0100

    Add files via upload

---
+++ trigo.h
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <math.h>
+
+int colltrigo(SDL_Rect rect1,SDL_Rect rect2);
+int ctrigoins(SDL_Rect rect1,SDL_Rect rect2);

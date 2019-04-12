947614bacbb54101ab75d9d6d7652bb634ed988c
aichakhlif <aicha.khlif@esprit.tn>
2019-04-11 17:41:31 -0700

    fichier collision parfaite

--- collision.h
+++ collision.h
@@ -1,10 +1,19 @@
-#ifndef ENNEMI_H_INCLUDED
-#define ENNEMI_H_INCLUDED
-#include "scrol.h"
-#include "ennemi.h"
+#ifndef COLLISION_H_INCLUDED
+#define COLLISION_H_INCLUDED
 
-SDL_Color GetPixel(SDL_Surface* pSurface, int x, int y);
-int collision_p1(SDL_Surface *back2,enemi e,scrol s);
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
 
-#endif
+#include "background.h"
+#include "personnage.h"
+SDL_Color GetPixel (SDL_Surface *pSurface , int x , int y);
+void update_points_de_collision(perssonage* hero);
+int collision_player_map_droite(background* acteurs , perssonage* hero);
+int collision_player_map_gauche(background* acteurs , perssonage* hero);
+int collision_player_map_top(background* acteurs , perssonage* hero);
+int collision_player_map_bas(background* acteurs , perssonage* hero);
 
+#endif // COLLISION_H_INCLUDED
